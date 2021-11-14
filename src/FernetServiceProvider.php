13a50e2dfd1e9bc60f765efcaa71bdf677295098
Erick Mercedes <erickmercedesemc@gmail.com>
2021-11-14 09:10:30 +0800

    Inicio del projecto

---
+++ src/FernetServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Erickmercedesc\Fernet;
+
+use Illuminate\Support\ServiceProvider;
+
+class FernetServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
